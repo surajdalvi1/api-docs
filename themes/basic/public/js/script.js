5eeeb0acfed4832ac2e7cc5c13ae15b5bb3f3a28
Suraj Dalvi <suraj.dalvi@raweng.com>
2017-02-28 19:17:30 +0530

    added pagetitle

--- themes/basic/public/js/script.js
+++ themes/basic/public/js/script.js
@@ -3,9 +3,11 @@
 $( document ).ready(function() {
 
     if(window.location.pathname=="/cda"){
+        $(".pagetitle").text("Content Delivery API");
         $(".cda").addClass("active");
         $(".cma").removeClass("active");
     }else if (window.location.pathname=="/cma") {
+        $(".pagetitle").text("Content Management API");
         $(".cda").removeClass("active");
         $(".cma").addClass("active");
     }else{
